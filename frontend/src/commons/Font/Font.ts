c80ddb27303814547fd1f6a4ed2562386a99ee68
DevLJW <jaewookv123@gmail.com>
2024-01-13 23:47:44 +0900

    font add

---
+++ frontend/src/commons/Font/Font.ts
@@ -0,0 +1,31 @@
+import localFont from "next/font/local";
+
+export const BMJUA = localFont({
+  src: [
+    {
+      path: "./NanumBrushScript-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+});
+
+export const BMHANNAPro = localFont({
+  src: [
+    {
+      path: "./BM/BMHANNA/BMHANNAPro.ttf",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+});
+
+export const BMDOHYEON = localFont({
+  src: [
+    {
+      path: "./BM/BMDOHYEON/BMDOHYEON_ttf.ttf",
+
+      style: "normal",
+    },
+  ],
+});
