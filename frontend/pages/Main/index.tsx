46c519bc5541a40ccb002fc49800c6e4f564e7e7
DevLJW <jaewookv123@gmail.com>
2024-01-22 23:17:34 +0900

    frontend main page add

---
+++ frontend/pages/Main/index.tsx
@@ -0,0 +1,6 @@
+import MainPage from "../../src/components/units/Main/Main.container";
+import Join from "../../src/components/units/User/Join/Join.container";
+
+export default function Main() {
+  return <MainPage></MainPage>;
+}
